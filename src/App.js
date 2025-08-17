ad7a885442fb75705e73f42b134b83cf06988f11
Kuba Karnath <jacob_jk@wp.pl>
2025-08-17 21:44:52 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const myDate = date.toLocaleString("pl-PL", {
+    minute: "numeric",
+    hour: "2-digit",
+  });
+  const mySecondDate = date.toLocaleString("pl-PL", {
+    day: "2-digit",
+    month: "numeric",
+    year: "numeric",
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="gora"></div>
+      <div className="srodek">
+        <div className="projekt">
+          <div className="profil"></div>
+        </div>
+        <div className="date">
+          {mySecondDate} {""}
+          {myDate}
+        </div>
+      </div>
+      <div className="dol" />
+      <div className="whitespace" />
+    </>
   );
 }
 
